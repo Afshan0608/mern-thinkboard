770eba8336be3fe346cdfa512bb3ef06689e70e6
afshan jabeen <afshanjabeen602@gmail.com>
2025-08-26 23:19:56 +0530

    rate limiter updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,6 +7,6 @@ import dotenv from "dotenv";
  //create a rate limiter that allows 5 requests per 10 seconds per IP address
  const ratelimit=new Ratelimit({   // Create a new rate limiter instance
     redis:Redis.fromEnv(), // Connect to Upstash Redis using environment variables
-    limiter:Ratelimit.slidingWindow(10,"20 s"), // Define the rate limit: 10 requests per 20 seconds
+    limiter:Ratelimit.slidingWindow(100,"60 s"), // Define the rate limit: 10 requests per 20 seconds
  });
 export default ratelimit; // Export the rate limiter instance for use in other parts of the application
